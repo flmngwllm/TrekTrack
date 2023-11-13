cd2e52f6429e91f1d006be2c9d5369fc2132d4db
flmngwllm <flmngwllm232@gmail.com>
2023-11-13 16:13:03 -0500

    created login and css styles

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <nav>
+      <AppNav />
+      <p>App</p>
+    </nav>
+  );
+}
+
+export default AppLayout;
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,12 +1,7 @@
-import Nav from "../components/Nav";
-
-function PageNotFound() {
+export default function PageNotFound() {
   return (
     <div>
-      <Nav />
-      <h1>Page Not Found</h1>
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
-
-export default PageNotFound;
