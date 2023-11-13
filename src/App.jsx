8c60710b8c221fb5631d94ff62fd27723fa7b28e
flmngwllm <flmngwllm232@gmail.com>
2023-11-13 14:46:44 -0500

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
+        <Route path="pricing" element={<Pricing />} />d
       </Routes>
     </BrowserRouter>
   );
