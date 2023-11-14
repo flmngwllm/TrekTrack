e24dbb2cf3a9d2059ce3ce531591808034914950
flmngwllm <flmngwllm232@gmail.com>
2023-11-13 19:22:21 -0500

    created city and country list

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,25 @@
+import CountryItem from "./CityItem";
+import styles from "./CountryList.module.css";
+import Message from "./Message";
+import Spinner from "./Spinner";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  const countries = [];
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
