83e9384232b65513d4e9187067fbbdbe9aeb2b2e
flmngwllm <flmngwllm232@gmail.com>
2023-11-14 13:58:09 -0500

    added form and programmatic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:9000";
 function App() {
@@ -44,13 +46,13 @@ function App() {
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
-          />{" "}
+          />
+          <Route path="cities/:id" element={<City />} />
           <Route
-            path="cities"
+            path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
-          />{" "}
-          <Route path="countries" element={<p>Countries</p>} />{" "}
-          <Route path="form" element={<p>Form</p>} />{" "}
+          />
+          <Route path="form" element={<Form />} />{" "}
         </Route>
 
         <Route path="*" element={<PageNotFound />} />
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,4 +1,4 @@
-import CountryItem from "./CityItem";
+import CountryItem from "./CountryItem";
 import styles from "./CountryList.module.css";
 import Message from "./Message";
 import Spinner from "./Spinner";
@@ -11,12 +11,16 @@ function CountryList({ cities, isLoading }) {
       <Message message="Add your first city by clicking on a city on the map" />
     );
 
-  const countries = [];
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
 
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem country={country} key={country.id} />
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );
