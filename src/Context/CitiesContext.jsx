989ab03d3c1cee1e5ba99e7f6dc4e1d6d9d72af1
flmngwllm <flmngwllm232@gmail.com>
2023-11-15 19:38:00 -0500

    added delete and added functionality to form

--- src/Context/CitiesContext.jsx
+++ src/Context/CitiesContext.jsx
@@ -37,8 +37,52 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      setCitites((cities) => [...cities, data]);
+      console.log(data);
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      setCitites((cities) => cities.filter((city) => city.id !== id));
+    } catch {
+      alert("There was an error");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
+    >
       {children}
     </CitiesContext.Provider>
   );
